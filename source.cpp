ec171b0e473fbd7c7b3a7b4a3f9b9cc9b9aae100
dtpvy <dtpvy0202@gmail.com>
2021-07-17 21:03:33 +0700

    Test gitignore

--- source.cpp
+++ source.cpp
@@ -7,6 +7,6 @@ int main()
     int n, *a;
     GenerateSortedData(a, n);
     output(Fibonaccisearch, a, n, a[5]);
-    cout << runningtime(Fibonaccisearch, a, n, a[5]);
+    cout << runningtime(Fibonaccisearch, a, n, a[n/2]);
     return 0;
 }
